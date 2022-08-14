87e09bf696d413d2f1d123fd4d25a80b912d9851
Serov Sergey <serovsergey79@gmail.com>
2022-08-14 11:31:26 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import '../css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import cardsTpl from '../templates/cards.hbs';
+
+// const { height: cardHeight } = document
+//   .querySelector(".gallery")
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: "smooth",
+// });
